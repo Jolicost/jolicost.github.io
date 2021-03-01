54d2418f957c44ab604d5b7268589626c7158240
jolivaco <jolivaco@uoc.edu>
2021-03-01 15:48:36 +0100

    cumple

---
+++ cumple/js/game_logic.js
@@ -0,0 +1,377 @@
+var dades = {
+    "supermercats":[
+        {
+            "nom":"Aldi",
+            "img-url":"images/supers/aldi.png",
+            "unlock":25
+        },
+        {
+            "nom":"BonÀrea",
+            "img-url":"images/supers/bonarea.png",
+            "unlock":25
+        },
+        {
+            "nom":"BonPreu",
+            "img-url":"images/supers/bonpreu.jpg",
+            "unlock":130
+        },
+        {
+            "nom":"Caprabo",
+            "img-url":"images/supers/caprabo.png",
+        },
+        {
+            "nom":"Carrefour",
+            "img-url":"images/supers/carrefour.png",
+        },
+        {
+            "nom":"Charter",
+            "img-url":"images/supers/charter.jpeg",
+        },
+        {
+            "nom":"Condis",
+            "img-url":"images/supers/condis.png",
+        },
+        {
+            "nom":"Consum",
+            "img-url":"images/supers/consum.jpg",
+            "unlock":40
+        },
+        {
+            "nom":"Dia",
+            "img-url":"images/supers/dia.jpg",
+            "unlock":25
+        },
+        {
+            "nom":"Lidl",
+            "img-url":"images/supers/lidl.png",
+            "unlock":65
+        },
+        {
+            "nom":"Mercadona",
+            "img-url":"images/supers/mercadona.jpg",
+            "unlock":65
+        },
+        {
+            "nom":"Altres",
+            "img-url":"images/supers/paquis.png",
+        },
+        {
+            "nom":"Spar",
+            "img-url":"images/supers/spar.jpg",
+        },
+        {
+            "nom":"Suma",
+            "img-url":"images/supers/suma.jpg",
+        },
+        {
+            "nom":"SuperMas",
+            "img-url":"images/supers/supermas.jpg",
+            "unlock": 105
+        }
+
+    ],
+    "personatges":[
+        {
+            "nom": "Aborrida", 
+            "img-url": "images/personatges/aborrit.jpg",
+            "desc": "Una persona que passa desaparcebuda i ho fa tot bé. Quin aborriment...",
+            "unlock":100,
+            "hab-lost":-5,
+            "habilitats": [
+                "No crida l’atenció.",
+                "És una persona civilitzada.",
+                "Paga el que compra."
+            ]
+        },
+        {
+            "nom": "Calçonassos", 
+            "img-url": "images/personatges/calconassos.jpg",
+            "desc": "Creu que la seva princesa té sempre raó.",
+            "hab-lost":-2,
+            "habilitats": [
+                "Només pot entrar al passadís de la neteja. ",
+                "Va amb els guants de rentar plats posats.",
+                "Camina amb el cul apretat del mal que li fa."
+            ]
+        },
+        {
+            "nom": "David", 
+            "img-url": "images/personatges/david.png",
+            "desc": "El David s'esforça una vegada a la seva vida.",
+            "unlock":50,
+            "hab-lost":-1,
+            "habilitats": [
+                "Camina amb desgana.",
+                "Sempre arriba l’últim a la caixa.",
+                "No pot carregar més d’un quilo."
+            ]
+        },
+        {
+            "nom": "Emancipao", 
+            "img-url": "images/personatges/emancipao.jpg",
+            "desc": "El emancipao acaba de deixar el niu. No sap el que l'espera...",
+            "hab-lost":-2,
+            "habilitats": [
+                "Paga sempre i únicament amb bitllets de 10.",
+                "Deixa sempre 1 cosa a caixa perquè no li arriben els calés.",
+                "Qualsevol producte que compri serà la marca més barata."
+            ]
+        },
+        {
+            "nom": "Estudiant de la sele", 
+            "img-url": "images/personatges/sele.jpg",
+            "desc": "El seu futur és més incert que la presència de kofels al lidl...",
+            "hab-lost":-2,
+            "habilitats": [
+                "Té pressa. No es pot estar parat més de 10 segons (tret de la cua).",
+                "Únicament pot anar pels passadissos on hi hagi café, xocolata, fideus instantanis i red bull. ",
+                "Un cop està a la cua canvia com a mínim 1 vegada cap a una caixa que li sembla més ràpida (si n’hi ha més d’una)"
+            ]
+        },
+        {
+            "nom": "Guiri", 
+            "img-url": "images/personatges/guiri.jpg",
+            "desc": "Tot just arribat d'Alemanya i amb ganes de mutar cap a tomàquet.",
+            "hab-lost":-1,
+            "habilitats": [
+                "Va al super amb xancletes i mitjons.",
+                "Porta ulleres de sol.",
+                "Només compra productes fabricats a la seva terra (alemanya, suïssa o països baixos, triar nacionalitat)"
+            ]
+        },
+        {
+            "nom": "Iaia", 
+            "img-url": "images/personatges/iaia.jpg",
+            "desc": "La iaia només vol veure el col·lapsament del món i de les persones, i per això fa un tap.",
+            "hab-lost":-2,
+            "unlock":25,
+            "habilitats": [
+                "No pot superar els 2km/h durant tota la visita al super. Es controlarà la velocitat amb un GPS.",
+                "Sempre s’aguanta amb el carret de ferro (el més gros possible) per no caure.",
+                "Només paga amb efectiu i sempre dona l’import just."
+            ]
+        },
+        {
+            "nom": "Influencer", 
+            "img-url": "images/personatges/influencer.jpg",
+            "desc": "La influencer es una estrella d'instagram i necessita cridar l'atenció per sobreviure.",
+            "hab-lost":-1,
+            "habilitats": [
+                "Cada vegada que entra a un super fa una review d’un producte com a mínim i la penja a instagram.",
+                "Es fa un selfie a costat de les amanides fent morritos (no la penja).",
+                "No pot carregar cap bossa ni cap article ja que és massa famosa."
+            ]
+        },
+        {
+            "nom": "Marisol", 
+            "img-url": "images/personatges/escombra.jpg",
+            "desc": "Com estalviar en la teva compra pel preu d'una gastrointeritis.",
+            "hab-lost":-1,
+            "habilitats": [
+                "Només compra productes caducats o a punt de caducar (màxim 1 mes).",
+                "Apareix al super 15 minuts abans de que tanqui.",
+                "No compra carn perquè diu que es vegana. Però pot comprar aliments que continguin carn de forma indirecta."
+            ]
+        },
+        {
+            "nom": "Maruja", 
+            "img-url": "images/personatges/maruja.jpg",
+            "desc": "La maruja es un ser que sap com defensarse en les rebaixes. Sol cridar molt i no li agrada que li parlin amb català",
+            "hab-lost":-2,
+            "habilitats": [
+                "La maruja sempre mira les revistes del cor quan entra a un super (no les compra).",
+                "No es desinfecta les mans a l'entrar al super.",
+                "La maruja provoca que el caixer sempre li parli amb castellà"
+            ]
+        },
+        {
+            "nom": "Perdut", 
+            "img-url": "images/personatges/naufrago.png",
+            "desc": "Sembla que sigui la primera vegada que el perdut entra en un supermercat.",
+            "hab-lost":-2,
+            "habilitats": [
+                "Si no troba el que vol en menys de dos minuts va a preguntar directament a un treballador.",
+                "Si no troba cap treballador en menys de dos minuts entra en pànic i abandona el supermercat.",
+                "Si ha de comprar fruita, no pesa cap peça abans d’anar per caixa."
+            ]
+        },
+        {
+            "nom": "Pija hippie", 
+            "img-url": "images/personatges/hippie.jpg",
+            "desc": "Adora els sers vius encara que el seu pare tingui un escorxador.",
+            "hab-lost":-2,
+            "habilitats": [
+                "Només pot comprar productes orgànics i bio.",
+                "Si ha de comprar carn, pregunta a la caixera si els animals han sigut feliços.",
+                "Sempre riu, per algun motiu desconegut (al·lucinògens)."
+            ]
+        },
+        {
+            "nom": "Sana", 
+            "img-url": "images/personatges/sano.jpg",
+            "desc": "La sana es llegeix totes les revistes de nutrició i es creu tot el que diuen, encara que es contradiguin.",
+            "hab-lost":-2,
+            "habilitats": [
+                "Es mira totes les etiquetes de les coses que agafa i diu en veu alta les calories.",
+                "No pot entrar al passadís de les patates fregides, la brioixeria, l’alcohol, els gelats ni les xocolates ni xuxes.",
+                "Quan passa els productes per caixa, pregunta una vegada si un article porta gluten."
+            ]
+        },
+        {
+            "nom": "Tete", 
+            "img-url": "images/personatges/tete.jpg",
+            "desc": "El tete es una espècie d'animal que conviu amb els sers humans. Ningú coneix com es reprodueixen tan ràpid.",
+            "hab-lost":-3,
+            "habilitats": [
+                "Només pot entrar als passadissos on hi han pizzes, cerveses i patates fregides.",
+                "Si es creua amb un altre tete ha de dir la paraula “tete” ni que sigui enmascarada en una altra paraula.",
+                "No pot comprar res superior a 5 euros."
+            ]
+        },
+        {
+            "nom": "TOC", 
+            "img-url": "images/personatges/toc.jpg",
+            "desc": "Les cortines no estàn del tot ben ficades...",
+            "unlock":50,
+            "hab-lost":-3,
+            "habilitats": [
+                "Va directament a les verdures i es fica 2 guants de plàstic a cada mà.",
+                "Fica els productes per caixa formant una cuadricula perfecta de tetris.",
+                "Camina en línies rectes paral·leles a les estanteries, mai fa diagonals."
+            ]
+        },
+        {
+            "nom": "Veterano del islote perejil", 
+            "img-url": "images/personatges/veterano.jpg",
+            "desc": "El veterano ha tornat d'una guerra que no va lluitar i te molts traumes per resoldre...",
+            "hab-lost":-1,
+            "habilitats": [
+                "Porta 4 xapes friquis a l’armilla que representen les seves guerres i batalles que no va lluitar per tenir els peus plans.",
+                "Fa la salutació militar davant dels fuets.",
+                "Porta boina."
+            ]
+        },
+        {
+            "nom": "Youtuber", 
+            "img-url": "images/personatges/youtuber.jpg",
+            "desc": "El youtuber no es desenganxa mai de les plataformes de streaming i sempre vol més suscriptors",
+            "hab-lost":-3,
+            "habilitats": [
+                "Graba tota la visita al super amb mòbil.",
+                "Porta gorra.",
+                "El youtuber sempre demana likes davant les pizzes congelades."
+            ]
+        },
+    ],
+    "totsReptes": [
+        {
+            "nom":"Nivell fàcil",
+            "reptes":[
+                {
+                    "desc":"fes el pi",
+                    "points": 40
+                },
+                {
+                    "desc":"fes el pi",
+                    "points": 40
+                }   
+            ]
+        },
+        {
+            "nom":"Nivell mitjà",
+            "reptes":[
+                {
+                    "desc":"fes el pi",
+                    "points": 40
+                },
+                {
+                    "desc":"fes el pi",
+                    "points": 40
+                }   
+            ]
+        },
+        {
+            "nom":"Nivell difícil",
+            "reptes":[
+                {
+                    "desc":"fes el pi",
+                    "points": 40
+                },
+                {
+                    "desc":"fes el pi",
+                    "points": 40
+                }   
+            ]
+        }      
+    ]
+};
+
+var points = 0;
+
+function refreshPoints() {
+    var elems = Array.from(document.getElementsByClassName("checked"));
+    var temp_points = 0;
+    elems.forEach(function(elem) {
+        var add_points = elem.getAttribute("add-points");
+        if (add_points) {
+            temp_points += parseInt(add_points);
+        }
+    });  
+    points = temp_points;
+    document.getElementById("points_tracker").innerHTML = points;
+}
+
+function addCheckableLogic() {
+    var elems = Array.from(document.getElementsByClassName("checkable"));
+    elems.forEach(function(elem) {
+        elem.onclick = function(e) {
+            this.classList.toggle("checked");
+            refreshPoints();
+            checkLocked();
+        };
+    });
+}
+
+function checkLocked() {
+    var elems = Array.from(document.querySelectorAll('[unlock]'));
+    elems.forEach(function(elem) {
+        var lockedPoints = parseInt(elem.getAttribute('unlock'));
+        if (lockedPoints) { 
+            if (lockedPoints >= points) 
+            {
+                elem.classList.add("locked");
+            }
+            else 
+            {
+                elem.classList.remove("locked"); 
+            }
+        } 
+    });
+}
+
+function toggleMarcador() {
+    var marcador = document.getElementById("marcador_text");
+    if (marcador.style.display == 'block') {   
+        marcador.style.display='none';
+    } else {
+        marcador.style.display = 'block';   
+    }
+}
+
+function renderLogic() {
+  var template = document.getElementById('supermercats-template-script').innerHTML;
+  var rendered = Mustache.render(template, dades);
+  document.getElementById('supermercats-template').innerHTML = rendered;
+  
+  var template = document.getElementById('personatges-template-script').innerHTML;
+  var rendered = Mustache.render(template, dades);
+  document.getElementById('personatges-template').innerHTML = rendered;
+  
+  var template = document.getElementById('reptes-template-script').innerHTML;
+  var rendered = Mustache.render(template, dades);
+  document.getElementById('reptes-template').innerHTML = rendered;
+  
+  addCheckableLogic();
+  checkLocked();
+  
+}
